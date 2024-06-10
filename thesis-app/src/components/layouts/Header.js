e33053d7751ad4128330d6c0082c66f672c810ff
MinhLong215 <148680972+MinhLong215@users.noreply.github.com>
2024-06-10 19:41:22 +0700

    chinh format ui thesisdetail, them navbar

--- thesis-app/src/components/layouts/Header.js
+++ thesis-app/src/components/layouts/Header.js
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import { Image, Nav, Navbar } from "react-bootstrap";
+import { Image, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../configs/UserContext";
-import cookie from 'react-cookies'
+import cookie from 'react-cookies';
 
 const getGreeting = () => {
   const currentHour = new Date().getHours();
@@ -27,37 +27,42 @@ const Header = () => {
   const handleLogout = () => {
     cookie.remove('token');
     cookie.remove('user');
-    dispatch({ "type": "logout" });
+    dispatch({ type: "logout" });
   };
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" style={{ padding: '8px 20px', height: '77px' }}>
+    <Navbar expand="lg" className="bg-light" style={{ padding: '8px 20px', height: '77px' }}>
       <Navbar.Brand as={Link} to="/">
         Thesis Management System
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Link to="/" className="nav-link">Trang chủ</Link>
-          {user ? <>
-            {user.account.role === 'AFFAIR' && (
+        <Nav className="me-auto mb-2 mb-lg-0">
+          <Link to="/" className="nav-link active" aria-current="page">Trang chủ</Link>
+          {user ? (
+            <>
+              {user.account.role === 'ADMIN' && (
+                <>
+                  <NavDropdown title="Đào tạo" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to="/admins/school-years">Niên khoá</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admins/faculties">Khoa đào tạo</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admins/majors">Ngành đào tạo</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admins/classes">Lớp học</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Người dùng" id="basic-nav-dropdown">
+                    <NavDropdown.Item as={Link} to="/admins/affairs">Giáo vụ</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admins/lecturers">Giảng viên</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admins/students">Sinh viên</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/admins">Quản trị viên</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              )}
               <Link to="/thesis" className="nav-link">Khoá luận</Link>
-            )}
-            <Link onClick={handleLogout} className="nav-link">Đăng xuất</Link>
-          </> : <>
+              <Link onClick={handleLogout} className="nav-link">Đăng xuất</Link>
+            </>
+          ) : (
             <Link to="/login" className="nav-link">Đăng nhập</Link>
-          </>}
-          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown> */}
+          )}
         </Nav>
         {user && (
           <Navbar.Text className="d-flex align-items-center ms-auto">
